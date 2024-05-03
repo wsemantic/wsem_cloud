6095889fc745e685e42f358ac760a51dad44a862
Alfonso Gonzalez <info@wsemantic.com>
2024-05-03 17:25:08 +0200

    Update models.py

--- cloud_crm/models/models.py
+++ cloud_crm/models/models.py
@@ -1,4 +1,7 @@
 from odoo import models, fields
+import logging
+
+_logger = logging.getLogger(__name__)
 
 #     @api.depends('value')
 #     def _value_pc(self):
@@ -6,6 +9,19 @@ from odoo import models, fields
 #             record.value2 = float(record.value) / 100
 
 
-#class AccountMove(models.Model):
-#    _inherit = 'account.move'
+class AuthSignupHome(models.Model):
+    _inherit = 'auth.signup.home'
+        
 
+    def _prepare_signup_values(self, qcontext):
+        _logger.info(f"WSEM Entrado en prepare {product.display_name}")
+        # Primero, obtener los valores originales llamando al método de la superclase
+        values = super(AuthSignupHome, self)._prepare_signup_values(qcontext)
+
+        # Ahora, añadir o modificar valores como sea necesario
+        if 'price' in qcontext:
+            values['price'] = qcontext.get('price')
+
+        return values
+
+        
