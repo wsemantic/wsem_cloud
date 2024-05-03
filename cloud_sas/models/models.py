538c943de9e9e6f2d378653ad5260bc4b1842f22
Alfonso Gonzalez <info@wsemantic.com>
2024-05-03 16:52:31 +0200

    wsem_cloud crm y sas inicial

---
+++ cloud_sas/models/models.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
+
+#class AccountMove(models.Model):
+#    _inherit = 'account.move'
+
