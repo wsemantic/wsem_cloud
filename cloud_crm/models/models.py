d52b06190cd3299d1acaa2a85e33b04cd1075133
Salvador <salvadorasquit@gmail.com>
2024-05-06 17:17:59 +0200

    Añadido el campo

--- cloud_crm/models/models.py
+++ cloud_crm/models/models.py
@@ -1,27 +1,10 @@
-from odoo import models, fields
-import logging
+from odoo import fields, models
 
-_logger = logging.getLogger(__name__)
+class ResUsersInherit(models.Model):
+    _inherit = 'res.users'
 
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
-
-class AuthSignupHome(models.Model):
-    _inherit = 'auth.signup.home'
-        
-
-    def _prepare_signup_values(self, qcontext):
-        _logger.info(f"WSEM Entrado en prepare {product.display_name}")
-        # Primero, obtener los valores originales llamando al método de la superclase
-        values = super(AuthSignupHome, self)._prepare_signup_values(qcontext)
-
-        # Ahora, añadir o modificar valores como sea necesario
-        if 'price' in qcontext:
-            values['price'] = qcontext.get('price')
-
-        return values
-
-        
+    price = fields.Selection([
+        ('community', 'Community'),
+        ('standard', 'Standard'),
+        ('custom', 'Custom')
+    ], string='Price')
