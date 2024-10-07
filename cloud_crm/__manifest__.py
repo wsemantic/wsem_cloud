3f56dbf983e77e7711ee6cc05b989ff34543cd86
Alfonso Gonzalez <info@wsemantic.com>
2024-10-07 20:17:44 +0200

    Update __manifest__.py

--- cloud_crm/__manifest__.py
+++ cloud_crm/__manifest__.py
@@ -25,9 +25,10 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/partner.xml',
+        
         'views/templates.xml',
-       
+        'views/sign_up_step1.xml',
+        'views/sign_up_step2.xml',
     ],
     "assets":{       
         'web.assets_frontend':[
