c12fed4f37d5628eb35afb35b7f977ed88ff6961
Alfonso Gonzalez <info@wsemantic.com>
2024-10-08 12:20:57 +0200

    Update signup_custom.py

--- cloud_crm/controllers/signup_custom.py
+++ cloud_crm/controllers/signup_custom.py
@@ -207,7 +207,7 @@ class CustomSignupController(http.Controller):
         subdomain = signup_data.get('subdomain')
 
         target_db = subdomain  # Usamos el subdominio como nombre de la base de datos
-        source_db = 'base_predefinida'  # Nombre de la base de datos predefinida a clonar
+        source_db = 'verifactu'  # Nombre de la base de datos predefinida a clonar
 
         # Leer la contraseña maestra desde el archivo de configuración de Odoo
         master_password = self.get_odoo_master_password()
