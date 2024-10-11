449ab11c09f2e1dbe14a2c7d27cdc1ca3b070e64
Alfonso Gonzalez <info@wsemantic.com>
2024-10-11 12:37:02 +0200

    Update controllers.py

--- cloud_sas/controllers/controllers.py
+++ cloud_sas/controllers/controllers.py
@@ -1,34 +1,25 @@
-import logging
-from odoo.http import request
 from odoo.addons.web.controllers.main import Home
-from odoo import http
+from odoo.http import request
+import logging
 
-# Crear un logger para el módulo
 _logger = logging.getLogger(__name__)
 
 class CustomHome(Home):
 
-    @http.route('/web', type='http', auth="user", website=True)
-    def web_client(self, s_action=None, **kw):
-        # Log para verificar si se ha accedido a la ruta /web
-        _logger.info("WSEM Accediendo a la ruta /web con el usuario: %s", request.env.user.name)
-        
-        # Verificar si el parámetro "debug" está presente en la URL
+    @classmethod
+    def _dispatch(cls):
+        # Log para verificar que estamos entrando en _dispatch
+        _logger.info("WSEM Entrando en _dispatch con el usuario: %s", request.env.user.name)
+
+        # Verificar si el parámetro 'debug' está presente
         if 'debug' in request.httprequest.args:
             _logger.info("WSEM Parámetro 'debug' encontrado en la URL.")
-            
-            # Verificar si el usuario tiene permisos para entrar en modo debug
+            # Verificar si el usuario pertenece al grupo factuadmin
             if not request.env.user.has_group('cloud_sas.factuadmin'):
-                _logger.warning("El usuario %s NO tiene permiso para activar el modo debug.", request.env.user.name)
-                
-                # Desactivar el modo debug
+                _logger.warning("WSEM El usuario %s NO tiene permiso para activar el modo debug.", request.env.user.name)
                 request.session.debug = ''
                 _logger.info("WSEM Modo debug desactivado para el usuario: %s", request.env.user.name)
-            else:
-                _logger.info("WSEM El usuario %s tiene permiso para activar el modo debug.", request.env.user.name)
-        else:
-            _logger.info("WSEM No se encontró el parámetro 'debug' en la URL.")
-        
-        # Llamar al método original de Home para continuar con el procesamiento estándar
-        _logger.info("Continuando con el procesamiento estándar del método 'web_client'.")
-        return super(CustomHome, self).web_client(s_action=s_action, **kw)
+
+        # Continuar con el comportamiento estándar
+        return super(CustomHome, cls)._dispatch()
+
