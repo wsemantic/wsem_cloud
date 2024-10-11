7338129838b09f719df8f896bd3fa3ae946402b2
Alfonso Gonzalez <info@wsemantic.com>
2024-10-11 12:47:18 +0200

    Update controllers.py

--- cloud_sas/controllers/controllers.py
+++ cloud_sas/controllers/controllers.py
@@ -1,32 +1,31 @@
+from odoo import http
+from odoo.http import request
 import logging
-from odoo.http import request, Response
-from odoo import models
 
 _logger = logging.getLogger(__name__)
 
-class CustomIrHttp(models.AbstractModel):
-    _inherit = 'ir.http'
+class CustomDebugController(http.Controller):
 
-    @classmethod
-    def _dispatch(cls, endpoint):
-        # Log para verificar que estamos entrando en _dispatch
-        _logger.info("WSEM Entrando en _dispatch con el usuario: %s", request.env.user.name)
+    @http.route('/web', type='http', auth="user", website=True)
+    def check_debug_mode(self, **kw):
+        # Log para verificar que se ha accedido a la ruta /web
+        _logger.info("WSEM - Accediendo a la ruta /web con el usuario: %s", request.env.user.name)
 
-        # Verificar si el parámetro 'debug' está presente en la URL
+        # Verificar si el parámetro "debug" está presente en la URL
         if 'debug' in request.httprequest.args:
-            _logger.info("WSEM Parámetro 'debug' encontrado en la URL.")
+            _logger.info("WSEM - Parámetro 'debug' encontrado en la URL.")
             
             # Verificar si el usuario pertenece al grupo factuadmin
             if not request.env.user.has_group('cloud_sas.factuadmin'):
-                _logger.warning("El usuario %s NO tiene permiso para activar el modo debug.", request.env.user.name)
+                _logger.warning("WSEM - El usuario %s NO tiene permiso para activar el modo debug.", request.env.user.name)
                 
                 # Desactivar el modo debug
                 request.session.debug = ''
-                _logger.info("WSEM Modo debug desactivado para el usuario: %s", request.env.user.name)
+                _logger.info("WSEM - Modo debug desactivado para el usuario: %s", request.env.user.name)
+            else:
+                _logger.info("WSEM - El usuario %s tiene permiso para activar el modo debug.", request.env.user.name)
+        else:
+            _logger.info("WSEM - No se encontró el parámetro 'debug' en la URL.")
 
-        # Continuar con el procesamiento original del método
-        result = endpoint(**request.params)
-        if isinstance(result, Response) and result.is_qweb:
-            result.flatten()
-        return result
-
+        # Llamar al método original para continuar con el procesamiento estándar
+        return request.render('web.webclient_bootstrap')
