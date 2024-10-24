a81716c6a0ff901515a82d98cd8a5414d8e75722
Alfonso Gonzalez <info@wsemantic.com>
2024-10-24 17:52:00 +0200

    Update signup_step1.js

--- cloud_crm/static/src/js/signup_step1.js
+++ cloud_crm/static/src/js/signup_step1.js
@@ -1,57 +1,53 @@
-odoo.define('cloud_crm.signup_step1', function (require) {
-    "use strict";
+/** @odoo-module */
 
-    const { Component, useState, mount } = require('web.owl');
-    const { rpc } = require('web.rpc');
-    const core = require('web.core');  // Necesario para cargar plantillas
+import { Component, useState } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 
-    // Cargar el template desde el archivo XML
-    const template = core.qweb.templates['cloud_crm.signup_step1_template'];
+class SignupStep1 extends Component {
+    setup() {
+        super.setup();
+        this.state = useState({
+            zip: '',
+            zipOptions: [],
+            selectedZip: null,
+            city: '',
+        });
+        
+        this.orm = useService("orm");
+    }
 
-    class SignupStep1 extends Component {
-        constructor() {
-            super(...arguments);
-            this.state = useState({
-                zip: '',
-                zipOptions: [],
-                selectedZip: null,
-                city: '',
-            });
-        }
+    async onZipInput(event) {
+        const term = event.target.value;
+        this.state.zip = term;
 
-        async onZipInput(event) {
-            const term = event.target.value;
-            this.state.zip = term;
-
-            if (term.length >= 2) {
-                const results = await rpc.query({
-                    model: 'res.city.zip',
-                    method: 'search_read',
-                    args: [[['name', 'ilike', term]], ['id', 'name', 'city_id']],
-                    limit: 10,
-                });
-
+        if (term.length >= 2) {
+            try {
+                const results = await this.orm.searchRead(
+                    'res.city.zip',
+                    [['name', 'ilike', term]],
+                    ['id', 'name', 'city_id'],
+                    { limit: 10 }
+                );
                 this.state.zipOptions = results;
-            } else {
+            } catch (error) {
+                console.error('Error fetching zip codes:', error);
                 this.state.zipOptions = [];
             }
-        }
-
-        onSelectZip(zip) {
-            this.state.selectedZip = zip;
+        } else {
             this.state.zipOptions = [];
-            this.state.city = zip.city_id[1];
-        }
-
-        static template = template;  // Asigna el template desde el XML cargado
-    }
-
-    function mountSignupStep1() {
-        const target = document.getElementById('signup_step1');
-        if (target) {
-            mount(SignupStep1, { target });
         }
     }
 
-    document.addEventListener('DOMContentLoaded', mountSignupStep1);
-});
+    onSelectZip(zip) {
+        this.state.selectedZip = zip;
+        this.state.zipOptions = [];
+        this.state.city = zip.city_id[1];
+    }
+
+    static template = 'cloud_crm.signup_step1_template';
+}
+
+registry.category("public_components").add("SignupStep1", SignupStep1);
+
+export default SignupStep1;
