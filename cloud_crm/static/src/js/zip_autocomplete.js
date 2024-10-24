207eb370dbf5363388c932c4c7934c8c26261a2d
Alfonso Gonzalez <info@wsemantic.com>
2024-10-24 16:52:38 +0200

    autocomplete OWL

--- cloud_crm/static/src/js/zip_autocomplete.js
+++ cloud_crm/static/src/js/zip_autocomplete.js
@@ -1,64 +1,63 @@
-// File: zip_autocomplete.js
-import { Component, useState } from owl;
-import { rpc } from 'web.rpc'; // para llamadas RPC en OWL
+odoo.define('cloud_crm.zip_autocomplete', function (require) {
+    "use strict";
 
-class ZipAutocomplete extends Component {
-    // Definimos el estado del componente
-    constructor() {
-        super(...arguments);
-        this.state = useState({
-            searchTerm: '',
-            results: [],
-            selectedZip: null,
-        });
-    }
+    var rpc = require('web.rpc');
+    var Component = require('owl.Component');
+    var useState = require('owl.hooks').useState;
 
-    // Método para manejar la búsqueda dinámica
-    async onSearchInput(ev) {
-        const term = ev.target.value;
-        this.state.searchTerm = term;
-
-        if (term.length >= 2) {
-            // Hacer la llamada RPC para obtener los códigos postales
-            const results = await rpc({
-                model: 'res.city.zip',
-                method: 'search_read',
-                args: [[['name', 'ilike', term]], ['id', 'name', 'city_id']],
-                limit: 10,
+    class ZipAutocomplete extends Component {
+        constructor() {
+            super(...arguments);
+            this.state = useState({
+                searchTerm: '',
+                results: [],
+                selectedZip: null,
             });
-            this.state.results = results;
-        } else {
+        }
+
+        async onSearchInput(ev) {
+            const term = ev.target.value;
+            this.state.searchTerm = term;
+
+            if (term.length >= 2) {
+                const results = await rpc.query({
+                    model: 'res.city.zip',
+                    method: 'search_read',
+                    args: [[['name', 'ilike', term]], ['id', 'name', 'city_id']],
+                    limit: 10,
+                });
+                this.state.results = results;
+            } else {
+                this.state.results = [];
+            }
+        }
+
+        onSelectZip(zip) {
+            this.state.selectedZip = zip;
             this.state.results = [];
+            this.trigger('zip-selected', { zip });
+        }
+
+        render() {
+            return (
+                <div>
+                    <input
+                        type="text"
+                        class="form-control"
+                        t-on-input="onSearchInput"
+                        placeholder="Ingrese el código postal o la ciudad"
+                    />
+                    <ul class="autocomplete-results">
+                        <t t-foreach="state.results" t-as="zip" t-key="zip.id">
+                            <li t-on-click="() => this.onSelectZip(zip)">
+                                <t t-esc="zip.name"/> - <t t-esc="zip.city_id[1]"/>
+                            </li>
+                        </t>
+                    </ul>
+                </div>
+            );
         }
     }
 
-    // Método para manejar la selección del código postal
-    onSelectZip(zip) {
-        this.state.selectedZip = zip;
-        this.state.results = [];
-        this.trigger('zip-selected', { zip });
-    }
-
-    // Renderizado del componente OWL
-    render() {
-        return (
-            <div>
-                <input
-                    type="text"
-                    class="form-control"
-                    t-on-input="onSearchInput"
-                    placeholder="Ingrese el código postal o la ciudad"
-                />
-                <ul class="autocomplete-results">
-                    <t t-foreach="state.results" t-as="zip" t-key="zip.id">
-                        <li t-on-click="() => this.onSelectZip(zip)">
-                            <t t-esc="zip.name"/> - <t t-esc="zip.city_id[1]"/>
-                        </li>
-                    </t>
-                </ul>
-            </div>
-        );
-    }
-}
-
-export default ZipAutocomplete;
+    return ZipAutocomplete;
+});
