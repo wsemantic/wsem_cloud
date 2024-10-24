bd308f8bdbcc821688365535a2f85101f1910a12
Alfonso Gonzalez <info@wsemantic.com>
2024-10-24 22:21:05 +0200

    split js

---
+++ cloud_crm/static/src/js/autocomplete_zip.js
@@ -0,0 +1,48 @@
+/** @odoo-module */
+
+import { Component, useState } from "@odoo/owl";
+import { xml } from "@odoo/owl";
+
+// Definir y exportar la clase del componente OWL
+export class ZipAutocomplete extends Component {
+    setup() {
+        this.state = useState({
+            zip: '',
+            zipOptions: [],
+            selectedZip: null,
+            city: '',
+        });
+    }
+
+    // Método para manejar la entrada del código postal
+    async onZipInput(event) {
+        const term = event.target.value;
+        this.state.zip = term;
+
+        if (term.length >= 2) {
+            const results = await this.env.services.rpc({
+                model: 'res.city.zip',
+                method: 'search_read',
+                args: [[['name', 'ilike', term]], ['id', 'name', 'city_id']],
+                limit: 10,
+            });
+
+            this.state.zipOptions = results;
+        } else {
+            this.state.zipOptions = [];
+        }
+    }
+
+    // Método para manejar la selección de un código postal
+    onSelectZip(zip) {
+        this.state.selectedZip = zip;
+        this.state.zipOptions = [];
+        this.state.city = zip.city_id[1];
+        // Disparar el evento para QWeb
+        this.trigger('zip-selected', { zip: zip });
+    }
+}
+
+// Asignación directa del template heredado
+// Asignar el template OWL desde el archivo XML
+ZipAutocomplete.template = 'signup_step1_template';
