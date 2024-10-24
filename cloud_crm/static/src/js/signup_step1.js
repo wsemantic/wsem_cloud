2a9b3a64eb5ece7a319b7a65c494b20664f3f354
Alfonso Gonzalez <info@wsemantic.com>
2024-10-24 18:01:09 +0200

    OWL

--- cloud_crm/static/src/js/signup_step1.js
+++ cloud_crm/static/src/js/signup_step1.js
@@ -1,35 +1,41 @@
-/** @odoo-module */
+/** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
+import { useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-import { useService } from "@web/core/utils/hooks";
 
-class SignupStep1 extends Component {
+export class SignupStep1 extends Component {
+    static template = 'cloud_crm.signup_step1_template';
+    
     setup() {
-        super.setup();
         this.state = useState({
             zip: '',
             zipOptions: [],
             selectedZip: null,
             city: '',
         });
-        
-        this.orm = useService("orm");
     }
 
-    async onZipInput(event) {
-        const term = event.target.value;
+    async onZipInput(ev) {
+        const term = ev.target.value;
         this.state.zip = term;
 
         if (term.length >= 2) {
             try {
-                const results = await this.orm.searchRead(
-                    'res.city.zip',
-                    [['name', 'ilike', term]],
-                    ['id', 'name', 'city_id'],
-                    { limit: 10 }
+                const response = await this.env.services.rpc(
+                    '/web/dataset/call_kw/res.city.zip/search_read',
+                    {
+                        model: 'res.city.zip',
+                        method: 'search_read',
+                        args: [],
+                        kwargs: {
+                            domain: [['name', 'ilike', term]],
+                            fields: ['id', 'name', 'city_id'],
+                            limit: 10,
+                        },
+                    }
                 );
-                this.state.zipOptions = results;
+                this.state.zipOptions = response;
             } catch (error) {
                 console.error('Error fetching zip codes:', error);
                 this.state.zipOptions = [];
@@ -44,10 +50,6 @@ class SignupStep1 extends Component {
         this.state.zipOptions = [];
         this.state.city = zip.city_id[1];
     }
-
-    static template = 'cloud_crm.signup_step1_template';
 }
 
 registry.category("public_components").add("SignupStep1", SignupStep1);
-
-export default SignupStep1;
