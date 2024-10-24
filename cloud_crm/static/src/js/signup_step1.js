250f9d08eb8a2e0a029f7da789fc08fd17350a5e
Alfonso Gonzalez <info@wsemantic.com>
2024-10-24 20:56:09 +0200

    Update signup_step1.js

--- cloud_crm/static/src/js/signup_step1.js
+++ cloud_crm/static/src/js/signup_step1.js
@@ -2,10 +2,9 @@
 
 import { Component, useState } from "@odoo/owl";
 import { xml } from "@odoo/owl";
-import { loadTemplate } from 'web.utils';
 
-// Definir el componente de autocompletado de códigos postales
-class ZipAutocomplete extends Component {
+// Definir y exportar la clase del componente OWL
+export class ZipAutocomplete extends Component {
     setup() {
         this.state = useState({
             zip: '',
@@ -42,15 +41,33 @@ class ZipAutocomplete extends Component {
         // Disparar el evento para QWeb
         this.trigger('zip-selected', { zip: zip });
     }
-
-    // Cargar y asignar el template desde el XML
-    static async willStart() {
-        const template = await loadTemplate("/cloud_crm/static/src/xml/owl_step1_template.xml");
-        this.template = template;
-    }
 }
 
-// No necesitas asignar el componente a `owl.component` manualmente
-// Exporta el componente para que Odoo lo registre automáticamente
-export default ZipAutocomplete;
-
+// Asignación directa del template heredado
+ZipAutocomplete.template = xml`
+    <div>
+        <!-- Input del Código Postal -->
+        <input
+            type="text"
+            class="form-control"
+            t-on-input="onZipInput"
+            placeholder="Ingrese el código postal"
+            t-model="state.zip"
+        />
+        <!-- Lista de Resultados de Autocompletado -->
+        <ul class="autocomplete-results">
+            <t t-foreach="state.zipOptions" t-as="zip" t-key="zip.id">
+                <li t-on-click="() => this.onSelectZip(zip)">
+                    <t t-esc="zip.name"/> - <t t-esc="zip.city_id[1]"/>
+                </li>
+            </t>
+        </ul>
+        <!-- Input de la Población -->
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Población"
+            t-model="state.city"
+        />
+    </div>
+`;
