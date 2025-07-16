3738de085fbe63b47e50409b6dd747e6dcd8ef6e
Jay Kotak <jay.kotak2021@gmail.com>
2025-07-17 03:18:27 +0530

    Restrict Debug & Log Warning

--- cloud_crm/models/models.py
+++ cloud_crm/models/models.py
@@ -17,9 +17,9 @@ class ResUsersInherit(models.Model):
     phone = fields.Char(string='phone')
     address = fields.Char(string='Address')
 
-    @api.model
-    def create(self, vals):
-        user = super(ResUsersInherit, self).create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        users = super(ResUsersInherit, self).create(vals_list)
 
         url = 'http://192.168.1.76:8069'
         db = 'wsem_verifactu_cloud'
@@ -27,18 +27,19 @@ class ResUsersInherit(models.Model):
         uid = 2
         models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
         
-        partner_id = models.execute_kw(db, uid, password, 'res.partner', 'create', [{
-            'name': vals.get('name', ''),  
-            'email': vals.get('email', ''),
-            'dni': vals.get('dni', ''),  
-            'phone': vals.get('phone', ''),
-            'address': vals.get('address', ''),  
-            'price': vals.get('price', ''),      
-        }])
+        for user, vals in zip(users, vals_list):
+            partner_id = models.execute_kw(db, uid, password, 'res.partner', 'create', [{
+                'name': vals.get('name', ''),  
+                'email': vals.get('email', ''),
+                'dni': vals.get('dni', ''),  
+                'phone': vals.get('phone', ''),
+                'address': vals.get('address', ''),  
+                'price': vals.get('price', ''),      
+            }])
 
-        user.partner_id = partner_id
+            user.partner_id = partner_id
 
-        _logger.info('Partner created with id: %s', partner_id)
-        _logger.info('User created with id: %s', user.id)
+            _logger.info('Partner created with id: %s', partner_id)
+            _logger.info('User created with id: %s', user.id)
 
-        return user
+        return users
