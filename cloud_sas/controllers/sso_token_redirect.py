60044eff6cd97b81554005a663bbef987b58479c
Alfonso Gonzalez <info@wsemantic.com>
2025-09-11 17:20:47 +0200

    Evitar conflicto de nombres con redirect en SSO

--- cloud_sas/controllers/sso_token_redirect.py
+++ cloud_sas/controllers/sso_token_redirect.py
@@ -1,24 +1,28 @@
 from odoo import http
 from odoo.http import request
 import time, json, base64, hmac, hashlib
-from werkzeug.utils import redirect
-SECRET_KEY = "e97b8f1debf497b4c3b67ac469b0d79fa1f47cc6d6f75c1467c2b637109a94e2" 
+from werkzeug.utils import redirect as werk_redirect
+
+SECRET_KEY = "e97b8f1debf497b4c3b67ac469b0d79fa1f47cc6d6f75c1467c2b637109a94e2"
+
 
 class SSORedirectController(http.Controller):
 
     def generate_token(self, login, expiration=300):
         data = {
-            'login': login,
-            'exp': time.time() + expiration,
+            "login": login,
+            "exp": time.time() + expiration,
         }
         payload = base64.urlsafe_b64encode(json.dumps(data).encode()).decode()
         signature = hmac.new(SECRET_KEY.encode(), payload.encode(), hashlib.sha256).hexdigest()
         return f"{payload}.{signature}"
 
-    @http.route('/sso/redirect', auth='user')
-    def sso_redirect(self, **kw):
+    @http.route(["/sso/redirect", "/sso/redirect/<path:redirect_path>"], auth="user")
+    def sso_redirect(self, redirect=None, redirect_path=None, **kw):
         login = request.env.user.login
         token = self.generate_token(login)
-        redirect_path = kw.get('redirect', '/my')
-        target_url = f"https://crm.factuoo.com/auth/sso_login?token={token}&redirect={redirect_path}"
-        return redirect(target_url)
+        target = redirect or redirect_path or "my"
+        target_url = (
+            f"https://crm.factuoo.com/auth/sso_login/{target.lstrip('/')}?token={token}"
+        )
+        return werk_redirect(target_url)
